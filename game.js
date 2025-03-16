c56cb50ade1a51843dd7c9030801885da17e9967
RIHO HORII <soad7447@gmail.com>
2025-03-16 21:39:45 +0900

    Add files via upload

--- game.js
+++ game.js
@@ -163,7 +163,7 @@ function endGame() {
   // 画面上部に表示されているスコアをそのまま使用
   const currentScore = document.getElementById("score").textContent;
   finalScore.textContent = currentScore.replace("スコア", "あなたのスコア");
-  maxScore.textContent = `最高可能スコア: ${maxPossibleScore}点`;
+  maxScore.textContent = `最高可能スコア: ${maxPossibleScore}`;
   endPopup.style.display = "flex";
 }
 
